0126ef5d219b8e6a784b9c7bb8402923d90e27f2
Jared Rulison <jared@Jareds-MacBook-Pro.local>
2017-10-20 18:12:22 -0700

    FIx merge conflicts.

--- blog-setup.sh
+++ blog-setup.sh
@@ -2,29 +2,10 @@
 
 sudo apt-get update
 
-# install java
-<<<<<<< HEAD
-sudo apt-get -y install default-jre
-
-# install javac
-sudo apt-get -y install default-jdk
-=======
-# sudo apt-get -y install default-jre
-
-# install javac
-# sudo apt-get -y install default-jdk
->>>>>>> cc2f72d76d07b65f5a3013df1e1114274e6af4c4
 
 # install python2.7
 sudo apt-get -y install python-minimal
 
-# one-time setup to read in training data and write blog model representation.
-<<<<<<< HEAD
-git clone https://github.com/BayesianLogic/blog
-cd blog
-sbt/sbt compile
-sbt/sbt stage
-=======
 git clone https://github.com/lileicc/swift
 cd swift
 
@@ -36,7 +17,7 @@ sudo apt-get -y install libarmadillo-dev
 
 make compile
 
-mv ../user_util.cpp .
-mv ../user_util.h .
->>>>>>> cc2f72d76d07b65f5a3013df1e1114274e6af4c4
+cp ../user_util.cpp .
+cp ../user_util.h .
+
 cd ..
